bac2589114bd823235660d42422b02d885b080a6
Asus <Asus@LAPTOP-06SLL2I5>
2023-01-18 14:20:17 +0700

    membuat frame button

---
+++ IEA-Psychology-Test/src/main/java/com/mycompany/iea/psychology/test/IEAPsychologyTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.iea.psychology.test;
+
+/**
+ *
+ * @author Muhammad Idris
+ * @author Ivanka Audy Ikhwan
+ * @author Tara Almanda
+ */
+public class IEAPsychologyTest {
+
+    public static void main(String[] args) {
+        Home HOME = new Home();
+        HOME.setLocationRelativeTo(null);
+        HOME.setVisible(true);
+    }
+}
